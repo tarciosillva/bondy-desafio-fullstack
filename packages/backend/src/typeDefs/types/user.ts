e186ac84adff31aea23c83e7c657aeee236aceed
tarciosillva <tarciosillva7@gmail.com>
2024-11-20 18:35:47 -0300

    feat: create a type User

---
+++ packages/backend/src/typeDefs/types/user.ts
@@ -0,0 +1,11 @@
+import gql from "graphql-tag";
+
+export default gql`
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        company: String
+        password: String!
+    }
+`
