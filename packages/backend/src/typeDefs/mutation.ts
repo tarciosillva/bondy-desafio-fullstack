3330fbe9b2b2650bb8d4e3e106a2fc2baf9c9cd8
tarciosillva <tarciosillva7@gmail.com>
2024-11-20 18:36:19 -0300

    feat: add login mutation ref

--- packages/backend/src/typeDefs/mutation.ts
+++ packages/backend/src/typeDefs/mutation.ts
@@ -1,7 +1,12 @@
 import gql from 'graphql-tag'
+import User from './types/user'
 
 export default gql`
   type Mutation {
     mutationTest(test: Boolean): Boolean
+    login(email: String!, password:String!): User
   }
+
+  ${User}
 `
+
